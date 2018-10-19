5fbdadd2e10f331c718039e5e463245ccb24d907
jiajianrong <jiajianrong@58ganji-corp.com>
2018-10-19 14:56:37 +0800

    3

--- my.js
+++ my.js
@@ -4,7 +4,7 @@ var fs = require('fs')
 
 for( var x=0; x<100000; x++ ) {
 
-	fs.appendFile('./backpressure.txt', x + '\r\n', 'utf8', 
+	fs.appendFile(  './backpressure.txt', x + '\r\n', 'utf8', 
                     function() {console.log(x)});
 
 }
